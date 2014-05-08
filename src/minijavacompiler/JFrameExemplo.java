d5e2a08678b64b837f5dfc4a780121fabcad5f6c
Eliton Luiz Scardin Perin <elitonluiz93@gmail.com>
2014-05-08 09:32:42 -0400

    Mudancas erradas

--- src/minijavacompiler/JFrameExemplo.java
+++ src/minijavacompiler/JFrameExemplo.java
@@ -2,6 +2,8 @@ package minijavacompiler;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -35,7 +37,17 @@ public class JFrameExemplo extends JFrame{
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(310, 360);
 		this.setVisible(true);
+		
+		addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				buttonConfirmaActionPerformed(e);	
+			}
 
 	}
+	
+	
 	
 }
